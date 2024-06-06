aa59d91abd67cf969fec9c9041109ddf31d508f6
ekaterine gurgenidze <ekateinegurgenidze@gmail.com>
2024-06-06 09:13:44 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import styled from 'styled-components';
+
+
+const StyledButton = styled.button`
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inlince-block;
+  font-size:16px;
+  cursor: pointer;
+  transition: 0.5s all ease-in;
+
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Button</button>
+      <StyledButton>Styled Button</StyledButton>
+      <h1>Hello <a href='/'> My Link</a> </h1>
+
+      <button class='my-butt'>My button</button>
+    
     </div>
   );
 }
